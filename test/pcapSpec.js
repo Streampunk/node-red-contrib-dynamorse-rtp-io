c430465cafb735387d3fcc1c8d4894833e0004ea
Simon Rogers <scriptorian@streampunk.media>
2017-01-16 12:14:57 +0000

    Fix depencies, start on tests

---
+++ test/pcapSpec.js
@@ -0,0 +1,57 @@
+/* Copyright 2016 Streampunk Media Ltd.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+var TestUtil = require('node-red-contrib-dynamorse-core').TestUtil;
+var util = require('util');
+
+var pcapTestNode = JSON.stringify({
+  "type": "pcap-reader",
+  "z": TestUtil.testFlowId,
+  "name": "pcap-reader-test",
+  "maxBuffer": 10,
+  "wsPort": TestUtil.properties.wsPort,
+  "x": 100.0,
+  "y": 100.0,
+  "wires": [[]]
+});
+var pcapNodeId = "24fde3d7.b7544c";
+var spoutNodeId = "f2186999.7e5f78";
+
+TestUtil.nodeRedTest('A pcap-reader->spout flow is posted to Node-RED', {
+  filename: __dirname + '/data/rtp-video-rfc4175-1080i50-colour.pcap',
+  maxBuffer: 10,
+  spoutTimeout: 0
+}, function getFlow(params) {
+  var testFlow = JSON.parse(TestUtil.testNodes.baseTestFlow);
+  testFlow.nodes[0] = JSON.parse(pcapTestNode);
+  testFlow.nodes[0].id = pcapNodeId;
+  testFlow.nodes[0].file = params.filename,
+  testFlow.nodes[0].maxBuffer = params.maxBuffer;
+  testFlow.nodes[0].wires[0][0] = spoutNodeId;
+
+  testFlow.nodes[1] = JSON.parse(TestUtil.testNodes.spoutTestNode);
+  testFlow.nodes[1].id = spoutNodeId;
+  testFlow.nodes[1].timeout = params.spoutTimeout;
+  return testFlow;
+}, function onMsg(t, params, msgObj, onEnd) {
+  //t.comment(`Message: ${JSON.stringify(msgObj)}`);
+  if (msgObj.hasOwnProperty('receive')) {
+    TestUtil.checkGrain(t, msgObj.receive);
+  }
+  else if (msgObj.hasOwnProperty('end') && (msgObj.src === 'spout')) {
+    onEnd();
+  }
+});
+
