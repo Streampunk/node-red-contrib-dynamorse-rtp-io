df5d96c6735147169be61c4817b10ca623609386
Simon Rogers <scriptorian@streampunk.media>
2018-04-11 09:28:39 +0200

    Fix testUtil

--- test/pcapSpec.js
+++ test/pcapSpec.js
@@ -13,7 +13,7 @@
   limitations under the License.
 */
 
-var TestUtil = require('dynamorse-test').TestUtil;
+var TestUtil = require('dynamorse-test');
 
 var pcapTestNode = JSON.stringify({
   'type': 'pcap-reader',
